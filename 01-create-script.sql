0b58c3a3694c0d3d114ea952a924d4c251f921e0
Prof-hu <prof.hu@gmail.com>
2017-06-07 23:54:36 +0200

    Fixing DDL

--- 01-create-script.sql
+++ 01-create-script.sql
@@ -3,21 +3,22 @@
  -- 03-query-readonly.sql
 
 -- Create keyspace (press TAB for autocomplete in cqlsh)
-CREATE KEYSPACE homework
+CREATE KEYSPACE homework WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
 
 -- Use keyspace
 USE homework;
 
 -- Fix the following table creation script so that the subsequent queries work as expected
 -- The table contains both the invoice header and the invoice detail fields
-CREATE table invoice
+CREATE TABLE invoice
 (
   -- header fields
   invoice_id text,
-  invoice_date date,
-  invoice_address text,
+  invoice_date date static,
+  invoice_address text static,
   -- detail fields
   line_id int,
   article_name text,
-  article_price decimal
+  article_price decimal,
+  PRIMARY KEY (invoice_id, line_id)
 );
